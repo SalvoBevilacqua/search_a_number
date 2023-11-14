6d06f3f2d26608868dfb317e034e50afb461fba6
SalvoBevilacqua <salvatorebevilacqua91@gmail.com>
2023-11-14 15:17:06 +0100

    update:logic

--- script.js
+++ script.js
@@ -11,16 +11,27 @@ console.log("il numero è " + center +" ?");
 
 maggiore.addEventListener("click", function() {
   inf = center;
-  center= center + ((sup - center) / 2);  
+  center= center + ((sup - center) / 2);
+  center = Math.round(center);
   console.log("il numero è " + center +" ?");
   console.log("inf: " + inf + " center: " + center + " sup: " + sup);
 })
 
 minore.addEventListener("click", function() {
   let temp = center;
-  center= center - ((sup - center) / 2);
+  
+  if ((sup - inf) < 3) {
+    center = inf + 1;
+  } else if ((sup - inf) < 5) {
+    sup = center;
+    center = inf + 2;  
+  } else {
+    center= center - ((sup - center) / 2);
+    center = Math.round(center);
+  }
+
   sup = temp;
-  // center= center / 2;
+
   console.log("il numero è " + center +" ?");
   console.log("inf: " + inf + " center: " + center + " sup: " + sup);
 })
