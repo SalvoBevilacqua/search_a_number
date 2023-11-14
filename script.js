f337d3438417a4c8d391c38071240e839bc19aae
SalvoBevilacqua <salvatorebevilacqua91@gmail.com>
2023-11-14 14:35:36 +0100

    fix:logic

--- script.js
+++ script.js
@@ -3,23 +3,24 @@ const maggiore = document.getElementById("maggiore");
 const minore = document.getElementById("minore");
 
 const max = 30000;
-let intervallo_sup = max;
-let intervallo_inf = intervallo_sup / 2;
-let result = 0;
+let sup = max;
+let inf = 0;
+let center = max / 2;
 
-console.log("il numero è " + max / 2 +" ?");
+console.log("il numero è " + center +" ?");
 
-maggiore.addEventListener("click", function() {  
-  result= (intervallo_sup - intervallo_inf) / 2 + intervallo_inf;
-  intervallo_inf = result;
-  console.log("il numero è " + result +" ?");
-  console.log(result, intervallo_inf, intervallo_sup);
+maggiore.addEventListener("click", function() {
+  inf = center;
+  center= center + ((sup - center) / 2);  
+  console.log("il numero è " + center +" ?");
+  console.log("inf: " + inf + " center: " + center + " sup: " + sup);
 })
 
 minore.addEventListener("click", function() {
-  intervallo_sup = intervallo_sup / 2;
-  result= (intervallo_sup - intervallo_inf) / 2;
-  intervallo_inf = result;
-  console.log("il numero è " + result +" ?");
-  console.log(result, intervallo_inf, intervallo_sup); 
+  let temp = center;
+  center= center - ((sup - center) / 2);
+  sup = temp;
+  // center= center / 2;
+  console.log("il numero è " + center +" ?");
+  console.log("inf: " + inf + " center: " + center + " sup: " + sup);
 })
